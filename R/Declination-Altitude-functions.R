eeb9421dec475f0ea18115a9d2b110343ebac54d
vreijs <web.victor.reijs@gmail.com>
2018-07-31 14:02:03 +0100

    some small changes (popped up when testing)

--- R/Declination-Altitude-functions.R
+++ R/Declination-Altitude-functions.R
@@ -1155,7 +1155,7 @@ S_HourAnglefromTopoAlt <-
       ),
       Lat
     )
-    retunr(Angle)
+    return(Angle)
   }
 
 ###################################################################
--- R/Periodicy.R
+++ R/Periodicy.R
@@ -500,6 +500,7 @@ S_HarmonicSum <- function (PeriodA, PeriodB, PlusMin = 1) {
 
 ###################################################################
 S_HD <- function(PeriodA, PeriodB) {
+  HSum <- S_HS(PeriodA, -PeriodB)
   return(HSum)
 }
 
@@ -587,7 +588,7 @@ TYVSOP <- function (Julmil) {
   #British Astronomical association, 102, 1, 1992
   YearL <-
     365.242189623 - 0.000061522 * Julmil - 0.0000000609 * Julmil ^ 2 + 0.00000026525 * Julmil ^ 3
-  retunr(YearL)
+  return(YearL)
 }
 
 ###################################################################
@@ -1092,7 +1093,7 @@ S_DraconicMonth <- function(JDNDays) {
   # using my own methodology of simple periods
   MonthL = S_HarmonicSum(S_LunarNodalCycle(JDNDays) * Y2D,
                          S_TropicalMonth(JDNDays),-1)
-  retunr(MonthL)
+  return(MonthL)
 }
 
 Eccentricity <- function (JDNDays) {
