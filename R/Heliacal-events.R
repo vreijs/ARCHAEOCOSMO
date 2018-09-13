848704163211b673e0933060f19986d194c027a2
vreijs <web.victor.reijs@gmail.com>
2018-09-13 17:30:04 +0100

    some clean-up

--- R/Heliacal-events.R
+++ R/Heliacal-events.R
@@ -201,7 +201,7 @@ S_HeliacalJDut <-
 #     # ' VR [km]
 #     # ' TypeAngle [0=heliacal, 1=Arcus Visonis, 2=Sun's altitude]
 #     # ' HeliacalAngle [deg]
-# 
+#
 #     if (PLSV == 1) {
 #       if (TypeAngle == 0) {
 #         Angle <- criticalangle
@@ -347,8 +347,8 @@ S_HeliacalJDut <-
 #     }
 #     return(Andle)
 #   }
-# 
-# 
+#
+#
 ###################################################################
 
 S_TopoArcVisionisSE <-
@@ -368,32 +368,25 @@ S_TopoArcVisionisSE <-
            RH,
            vr,
            Magnify = 1) {
-    # Dim serr As String
-    # Dim dgeo(2) As Double
-    # Dim dobs(5) As Double
-    # Dim datm(3) As Double
-    # Dim helflag As Long
-
     dgeo <- c(0, Lat, HeightEye)
     datm <- c(pressure, Temperature, RH, vr)
     dobs <-
       c(Age, SN, GBinocular, Magnify, Magnify * 5, GOpticTrans)
-
-    helflag <- SE_HELIACAL_HIGH_PRECISION + SE_HELIACAL_OPTICAL_PARAMS
+    
+    helflag <-
+      SE_HELIACAL_HIGH_PRECISION + SE_HELIACAL_OPTICAL_PARAMS
     i <-
-      swe_topo_arcus_visionis(
-        JDNDaysUT,
-        dgeo,
-        datm,
-        dobs,
-        helflag,
-        Magn,
-        AziO,
-        AltO,
-        AziS,
-        AziM,
-        AltM
-      )
+      swe_topo_arcus_visionis(JDNDaysUT,
+                              dgeo,
+                              datm,
+                              dobs,
+                              helflag,
+                              Magn,
+                              AziO,
+                              AltO,
+                              AziS,
+                              AziM,
+                              AltM)
     if (i$return == 0) {
       TAV = i$tav
     }
